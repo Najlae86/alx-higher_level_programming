511e88d8296c30c41fb5933391e33d82834d8c51
AISSAOUI Najlae <najlae.aissaoui86@gmail.com>
2023-06-08 15:12:09 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+n = len(sys.argv) - 1
+if n == 0:
+    print("0 arguments.")
+elif n == 1:
+    print("1 arguments:")
+else:
+    print("{} arguments:".format(n))
+for i in range(n):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
