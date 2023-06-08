cd984cde9779a12d01679ef53c96ad00a59d6fd4
AISSAOUI Najlae <najlae.aissaoui86@gmail.com>
2023-06-08 15:22:10 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-__name__ == "__main__":
+if __name__ == "__main__":
     import sys
-    count = len(sys.argv) - 1
-    if count == 0:
+    n = len(sys.argv) - 1
+    if n == 0:
         print("0 arguments.")
-    elif count == 1:
+    elif n == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
+        print("{} arguments:".format(n))
     for i in range(n):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
