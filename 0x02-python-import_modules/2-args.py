4cbdfbfe64ace3184b45989d7827c49545716fa1
AISSAOUI Najlae <najlae.aissaoui86@gmail.com>
2023-06-08 15:15:05 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ n = len(sys.argv) - 1
 if n == 0:
     print("0 arguments.")
 elif n == 1:
-    print("1 arguments:")
+    print("1 argument:")
 else:
     print("{} arguments:".format(n))
 for i in range(n):
