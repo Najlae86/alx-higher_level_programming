4d6024521ab63f804d58bf277c786e36655d1904
AISSAOUI Najlae <najlae.aissaoui86@gmail.com>
2023-08-20 15:44:19 -0700

    16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- Lists all of the second_table's records having a name value.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `score_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;
