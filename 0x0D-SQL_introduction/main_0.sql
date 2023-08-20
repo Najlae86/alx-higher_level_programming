a584a8cafd86530db97ae923992e3fcf2795e837
AISSAOUI Najlae <najlae.aissaoui86@gmail.com>
2023-08-20 15:46:26 -0700

    16-no_link.sql

--- 0x0D-SQL_introduction/main_0.sql
+++ 0x0D-SQL_introduction/main_0.sql
@@ -11,13 +11,13 @@ CREATE TABLE IF NOT EXISTS second_table (
 );
 
 -- Create records
-INSERT INTO second_table (id, score) VALUES (1, 12);
+INSERT INTO second_table (id, name, score) VALUES (1, "A", 12);
 INSERT INTO second_table (id, name, score) VALUES (2, "B", 2);
 INSERT INTO second_table (id, name, score) VALUES (3, "C", -12);
-INSERT INTO second_table (id, score) VALUES (4, 89);
+INSERT INTO second_table (id, name, score) VALUES (4, "D", 89);
 INSERT INTO second_table (id, name, score) VALUES (5, "E", 0);
 INSERT INTO second_table (id, name, score) VALUES (6, "F", 23);
 INSERT INTO second_table (id, score) VALUES (7, 5);
 INSERT INTO second_table (id, name, score) VALUES (8, "H", 6);
-INSERT INTO second_table (id, score) VALUES (9, -9);
+INSERT INTO second_table (id, name, score) VALUES (9, "I", -9);
 INSERT INTO second_table (id, name, score) VALUES (10, "J", 3);
