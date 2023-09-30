77d90b0df19dfe062696c99480f995c046e5dea5
Najlae86 <najlae.aissaoui86@gmail.com>
2023-09-30 10:46:47 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,8 +6,8 @@ import sys
 import requests
 
 
-if __name__ == '__main__':
-    letter = "" if len(sys.argv) == 1 else sys.agrv[1]
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
@@ -16,6 +16,6 @@ if __name__ == '__main__':
         if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), reponse.get("name")))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
