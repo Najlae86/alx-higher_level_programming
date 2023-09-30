fef66c644d7155c49e77c5a3bed2c1c78442c958
Najlae86 <najlae.aissaoui86@gmail.com>
2023-09-30 09:14:19 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.htbn.io/status."""
-import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.htbn.io/status")
-    with urllib.request.urlopen(request) as reqponse:
-        body = reqponse.read()
+    import urllib.request
+
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
