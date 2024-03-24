697e7559bf4a041d559b11252f7563fbdf8d1284
Najlae86 <najlae.aissaoui86@gmail.com>
2024-03-24 10:29:31 -0700

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,7 +3,7 @@
 Python script that fetches an URL with requests package
 """
 import requests
-
+import sys
 
 if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
