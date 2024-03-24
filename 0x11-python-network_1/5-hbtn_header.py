e21b901ac5d9cf9276914bc34f33bd2a59248805
Najlae86 <najlae.aissaoui86@gmail.com>
2024-03-24 10:32:12 -0700

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,6 +6,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
