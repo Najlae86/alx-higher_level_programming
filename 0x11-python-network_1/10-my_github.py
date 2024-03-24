40903102952d93ff95496ce1ddc10fb3f2688511
Najlae86 <najlae.aissaoui86@gmail.com>
2024-03-24 10:39:53 -0700

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,12 +5,10 @@ Star Wars API, searching people
 """
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    search = 'https://swapi.co/api/people/?search={}'.format(sys.argv[1])
-    r = requests.get(search)
-    json_o = r.json()
-    print("Number of results: {}".format(json_o.get('count')))
-    for character in json_o.get('results'):
-        print(character.get('name'))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
